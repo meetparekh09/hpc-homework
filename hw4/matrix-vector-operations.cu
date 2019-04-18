9dd513ff9e0c0573e1d96a9d562f4f26a4f3cf93
meetparekh09 <meetparekh09@gmail.com>
2019-04-18 19:20:38 -0400

    Matrix Vector Operations Done

--- hw4/matrix-vector-operations.cu
+++ hw4/matrix-vector-operations.cu
@@ -4,6 +4,7 @@
 
 
 #define BLOCK_SIZE 1024
+#define MULTIDIM_BLOCK_SIZE 32
 
 void inner_product(double *x, double *y, double *ans_ptr, long N) {
     double ans = 0.0;
@@ -12,6 +13,19 @@ void inner_product(double *x, double *y, double *ans_ptr, long N) {
         ans += x[i]*y[i];
     }
     *ans_ptr = ans;
+}
+
+
+void matrix_vector_mult(double *x, double *y, double *ans_ptr, long M, long N) {
+    #pragma omp parallel for
+    for(int i = 0; i < M; i++) {
+        double ans = 0.0;
+        #pragma omp parallel for reduction(+:ans)
+        for(int j = 0; j < N; j++) {
+            ans += x[N*i + j] * y[j];
+        }
+        ans_ptr[i] = ans;
+    }
 }
 
 
@@ -52,8 +66,16 @@ __global__ void inner_product_multiply_kernel(double *a, double *b, double *c, l
     if(idx < N) c[idx] = a[idx] * b[idx];
 }
 
+__global__ void matrix_vector_mult_kernel(double *a, double *b, double *c, long M, long N) {
+    int x_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int y_idx = blockIdx.y * blockDim.y + threadIdx.y;
 
-int main() {
+    // printf("%d %d\n", x_idx, y_idx);
+
+    if(x_idx < N && y_idx < M) c[y_idx * N + x_idx] = a[y_idx * N + x_idx] * b[x_idx];
+}
+
+void execute_dot_product() {
     long N = (1UL<<25);
     double *x, *y;
 
@@ -115,5 +137,92 @@ int main() {
     printf("GPU Bandwidth = %f GB/s\n", 1*N*sizeof(double) / (te - tt)/1e9);
     printf("Error = %f\n", fabs(ans-ans_ref));
 
-    //printf("%ld\n", N);
+}
+
+void execute_matrix_vector_multiplication() {
+    long M = (1UL<<13);
+    long N = (1UL<<13);
+
+    double *x, *y, *ans_ref;
+
+    cudaMallocHost((void**)&x, M * N * sizeof(double));
+    cudaMallocHost((void**)&y, N * sizeof(double));
+    cudaMallocHost((void**)&ans_ref, M * sizeof(double));
+
+    #pragma omp parallel for
+    for(int j = 0; j < N; j++) {
+        for(long i = 0; i < M; i++) {
+            x[N*i + j] = i + j;
+        }
+        y[j] = j;
+    }
+
+    double tt = omp_get_wtime();
+    matrix_vector_mult(x, y, ans_ref, M, N);
+    double te = omp_get_wtime();
+
+    printf("CPU Performance = %f GFLOPs\n", 2*M*N*sizeof(double) / 1e9 / (te-tt));
+    printf("CPU Bandwidth = %f GB/s\n", 1*M*N*sizeof(double) / (te-tt)/1e9);
+
+
+    double *a, *b, *c, *c_h, *sum, *sum_d;
+    cudaMalloc(&a, M * N * sizeof(double));
+    cudaMalloc(&b, N * sizeof(double));
+    cudaMalloc(&c, M * N * sizeof(double));
+    cudaMallocHost((void**)&c_h, M * sizeof(double));
+
+    long N_work = 1;
+    for (long j = (N+BLOCK_SIZE-1)/(BLOCK_SIZE); j > 1; j = (j+BLOCK_SIZE-1)/(BLOCK_SIZE)) N_work += j;
+    cudaMalloc(&sum, N_work*sizeof(double));
+
+    cudaMemcpyAsync(a, x, M * N * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpyAsync(b, y, N * sizeof(double), cudaMemcpyHostToDevice);
+
+    dim3 dimGrid((M + MULTIDIM_BLOCK_SIZE - 1) / MULTIDIM_BLOCK_SIZE, (N + MULTIDIM_BLOCK_SIZE - 1) / MULTIDIM_BLOCK_SIZE);
+    dim3 dimBlock(MULTIDIM_BLOCK_SIZE, MULTIDIM_BLOCK_SIZE);
+
+
+    tt = omp_get_wtime();
+
+    matrix_vector_mult_kernel<<<dimGrid, dimBlock>>>(a, b, c, M, N);
+
+    for(int i = 0; i < M; i++) {
+        long N_temp = N;
+
+        sum_d = sum;
+        long Nb = (N_temp + BLOCK_SIZE - 1)/BLOCK_SIZE;
+        reduction_kernel2<<<Nb, BLOCK_SIZE>>>(sum_d, c + i*N, N_temp);
+
+        while(Nb > 1) {
+            N_temp = Nb;
+            Nb = (Nb + BLOCK_SIZE - 1)/BLOCK_SIZE;
+            reduction_kernel2<<<Nb, BLOCK_SIZE>>>(sum_d + N_temp, sum_d, N_temp);
+            sum_d += N_temp;
+        }
+
+        cudaDeviceSynchronize();
+        cudaMemcpyAsync(c_h + i, sum_d, 1*sizeof(double), cudaMemcpyDeviceToHost);
+    }
+
+    cudaDeviceSynchronize();
+    te = omp_get_wtime();
+    printf("GPU Performance = %f GFLOPs\n", 2*M*N*sizeof(double) / 1e9 / (te-tt));
+    printf("GPU Bandwidth = %f GB/s\n", 1*M*N*sizeof(double) / (te - tt)/1e9);
+
+    double error = 0.0;
+    for(int i = 0; i < M; i++) {
+        error += fabs(c_h[i] - ans_ref[i]);
+    }
+
+    printf("Error: %f\n", error);
+}
+
+int main() {
+    printf("Taking Inner Product :: \n");
+    execute_dot_product();
+
+    printf("\n\n ==================================================================================== \n\n");
+
+    printf("Taking Matrix Vector Product :: \n");
+    execute_matrix_vector_multiplication();
 }
