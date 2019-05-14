6a0194f0b8b874cfcc5d8c09daf3c7240068b5bb
meetparekh09 <meetparekh09@gmail.com>
2019-05-13 21:08:31 -0400

    Sample sort completed

--- hw6/ssort.cpp
+++ hw6/ssort.cpp
@@ -16,7 +16,7 @@ int main( int argc, char *argv[]) {
 
   // Number of random numbers per processor (this should be increased
   // for actual tests or could be passed in through the command line
-  int N = 10000;
+  int N = 1000000;
 
   int* vec = (int*)malloc(N*sizeof(int));
   int *samples;
@@ -43,6 +43,14 @@ int main( int argc, char *argv[]) {
   // sort locally
   std::sort(vec, vec+N);
 
+  // MPI_Barrier(MPI_COMM_WORLD);
+  //
+  // for(int i = 0; i < N; i++) {
+  //     printf("%d:: %d => %d\n", rank, i, vec[i]);
+  // }
+  //
+  // MPI_Barrier(MPI_COMM_WORLD);
+
   // sample p-1 entries from vector as the local splitters, i.e.,
   // every N/P-th entry of the sorted vector
   int dist = (int)round(((double)N)/((double)(p)));
@@ -54,6 +62,7 @@ int main( int argc, char *argv[]) {
       sample[j++] = vec[i];
       if(j == p-1) break;
   }
+  // printf("%d %d\n", rank, j);
 
 
   // every process communicates the selected entries to the root
@@ -78,6 +87,15 @@ int main( int argc, char *argv[]) {
   // root process broadcasts splitters to all other processes
   MPI_Bcast(splitters, p-1, MPI_INT, root, MPI_COMM_WORLD);
 
+  // if(rank == root) {
+  // printf("\n\nSplitters\n");
+  // for(int i = 0; i < p-1; i++) {
+  //     printf("%d\n", splitters[i]);
+  // }
+  // printf("\n\n");
+  // }
+  // MPI_Barrier(MPI_COMM_WORLD);
+
   // every process uses the obtained splitters to decide which
   // integers need to be sent to which other process (local bins).
   // Note that the vector is already locally sorted and so are the
@@ -94,21 +112,47 @@ int main( int argc, char *argv[]) {
   for(int i = 1; i < p; i++) {
       sdispls[i] = std::lower_bound(vec, vec+N, splitters[i-1]) - vec;
       if(i == 1) scounts[i-1] = sdispls[i] - sdispls[i-1];
-      else scounts[i-1] = sdispls[i] - sdispls[i-1] + 1;
+      else scounts[i-1] = sdispls[i] - sdispls[i-1];
   }
   scounts[p-1] = N - sdispls[p-1];
 
+  int total = 0;
+  for(int i = 0; i < p; i++) {
+      total += scounts[i];
+  }
+
+  // MPI_Barrier(MPI_COMM_WORLD);
+  //
+  // printf("rank to_rank sdispls scounts\n");
+  // for(int i = 0; i < p; i++) {
+  //     printf("%d %d %d %d\n", rank, i, sdispls[i], scounts[i]);
+  // }
+  //
+  // MPI_Barrier(MPI_COMM_WORLD);
 
   // send and receive: first use an MPI_Alltoall to share with every
   // process how many integers it should expect, and then use
   // MPI_Alltoallv to exchange the data
   MPI_Alltoall(scounts, 1, MPI_INT, rcounts, 1, MPI_INT, MPI_COMM_WORLD);
-  int total = 0;
+  total = 0;
   for(int i = 0; i < p; i++) {
       if(i == 0) rdispls[i] = 0;
-      else rdispls[i] = rdispls[i-1] + rcounts[i] ;
+      else rdispls[i] = rdispls[i-1] + rcounts[i-1];
       total += rcounts[i];
   }
+
+  // MPI_Barrier(MPI_COMM_WORLD);
+  //
+  // printf("\n\n");
+  // printf("rank from_rank rdispls rcounts\n");
+  // for(int i = 0; i < p; i++) {
+  //     printf("%d %d %d %d\n", rank, i, rdispls[i], rcounts[i]);
+  // }
+  // printf("\n\n");
+  //
+  // MPI_Barrier(MPI_COMM_WORLD);
+
+  // printf("Rank %d :: Count %d\n", rank, total);
 
   bucket = (int*)malloc(total*sizeof(int));
 
@@ -121,25 +165,25 @@ int main( int argc, char *argv[]) {
   double t2 = MPI_Wtime();
 
   if(rank == root) {
-      printf("Time taken for N = 10000 and 64 Cores :: %f\n", t2-t1);
+      printf("Time taken for N = %d and %d Cores :: %f\n", N, p, t2-t1);
   }
 
 
   // every process writes its result to a file
-  // FILE* fd = NULL;
-  // char filename[256];
-  // snprintf(filename, 256, "output%02d.txt", rank);
-  // fd = fopen(filename,"w+");
-  //
-  // if(NULL == fd) {
-  //   printf("Error opening file \n");
-  //   return 1;
-  // }
-  //
-  // for(int i = 0; i < total; i++)
-  //   fprintf(fd, "  %d\n", bucket[i]);
-  //
-  // fclose(fd);
+  FILE* fd = NULL;
+  char filename[256];
+  snprintf(filename, 256, "output%02d.txt", rank);
+  fd = fopen(filename,"w+");
+
+  if(NULL == fd) {
+    printf("Error opening file \n");
+    return 1;
+  }
+
+  for(int i = 0; i < total; i++)
+    fprintf(fd, "  %d\n", bucket[i]);
+
+  fclose(fd);
 
   free(vec);
   free(sample);
